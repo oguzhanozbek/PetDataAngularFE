08a077aa13dc18759cba7cc59f0663bcb0480176
Oguzhan Ozbek <ozbeko@sheridancollege.ca>
2023-04-14 14:24:39 -0400

    The userlist and userdetails components have been imported into AppRoutingModule. Also, the routes have been defined.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserlistComponent } from './userlist/userlist.component';
+import { UserdetailsComponent } from './userdetails/userdetails.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: UserlistComponent},
+  {path:'users/:name', component:UserdetailsComponent},
+  {path:'**', redirectTo:''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
